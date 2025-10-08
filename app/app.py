4301f559763e291fd10bacf5883c9c8e927d0994
hervertJ <hervertjimenez1@gmail.com>
2025-10-08 16:41:25 -0500

    Update

--- app/app.py
+++ app/app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    # return "12345"
+    data = {"titulo": "Index", "bienvenida": "Saludsos"}
+    return render_template("index.html", data=data)
+
+
+@app.route("/contacto/<nombre>")
+def contacto(nombre):
+    data = {"titulo": "contacto", "nombre": nombre}
+    return render_template("contanto.html")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5000)
